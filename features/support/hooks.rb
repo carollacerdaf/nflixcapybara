cbbd314f0becac11f1c19355ba6ae3465617459c
clbatista <clbatista@stefanini.com>
2020-05-26 13:12:51 -0300

    ReportBuilder

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,6 @@
+require "report_builder"
+require "date"
+
 Before do
     @login_page = LoginPage.new
     @movie_page = MoviePage.new
@@ -16,7 +19,23 @@ end
 After do |scenario|
     if scenario.failed?
         temp_shot = page.save_screenshot("log/temp_shot.png")
-        screenshot = Base64.encode64(File.open(temp_shot).read)
+        screenshot = Base64.encode64(File.open(temp_shot, "rb").read)
         embed(screenshot, "image/png", "Screenshot")
     end
+end
+
+ d = DateTime.now
+ @current_date = d.to_s.tr(":","-")
+
+at_exit do
+    ReportBuilder.configure do |config|
+      config.input_path = "log/report.json"
+      config.report_path = "log/" + @current_date
+      config.report_types = [:retry, :html]
+      config.report_title = "Ninja Flix -  WebApp"
+      config.compress_image = true
+      config.additional_info = {"App" => "Web", "Data de Execução" => @current_date}
+      config.color = "indigo"
+    end
+    ReportBuilder.build_report
 end
