ad0a392a245e3b4657f6e5c14beeea32390a6dfb
clbatista <clbatista@stefanini.com>
2020-05-26 10:48:39 -0300

    ENV variables

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,24 @@ require_relative "helpers"
 
 World(Helpers)
 
+CONFIG = ENV["ENV_TYPE"]
+
+CONFIG = YAML.load_file(File.join(Dir.pwd, "feature/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+
+case ENV["BROWSER"]
+when "firefox"
+    @driver= :selenium
+when "chrome"
+    @driver=:selenium_chrome
+when "headless"
+    @driver=:selenium_chrome_headless
+else
+    puts "Invalid Browser"
+
+#comand line: cucumber -t @login_happy -p dev_toolbox
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = "http://localhost:8080"
-    config.default_max_wait_time = 10
+    config.default_driver = @driver
+    config.app_host = CONFIG["url"]
+    config.default_max_wait_time = 5
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,14 @@
 Before do
     @login_page = LoginPage.new
     @movie_page = MoviePage.new
-    @sidebar = SideBarView.new    
+    @sidebar = SideBarView.new
+    
+    #page.driver.browser.manage.window.maximize
+    page.current_window.resize_to(1440, 900)
 end
 
 Before("@login") do
+    user = CONFIG["users"]["cat_manager"]
     @login_page.go
-    @login_page.with('carol@mail.com','pwd123')
+    @login_page.with(user["email"],user["pass"])
 end
